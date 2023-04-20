023f3e51d8a94dbe7d071464e533726c2447098c
Fortune Ngwenya <fortune.ngwenya@platform45.com>
2023-04-21 01:05:22 +0200

    update

--- Controllers/V2CatController.cs
+++ Controllers/V2CatController.cs
@@ -8,14 +8,14 @@ namespace Example.Controllers
 	[ApiController]
 	public class V2CatController : ControllerBase
 	{
-		[HttpPut("/./v2/v2cat/{namepace}/{name}")]
+		[HttpPut("/./v2/v2cat/{namespace}/{name}")]
 		public V2CatStatus Put(string @namespace, string name, V2Cat v2cat)
 		{
 			Console.WriteLine($"\n{Request.Method} {Request.Path}\nnamespace : {@namespace}\nname : {name}\nrequest : {JsonConvert.SerializeObject(v2cat, Formatting.Indented)}");
 			return v2cat.Status;
 		}
 
-		[HttpDelete("/./v2/v2cat/{namepace}/{name}")]
+		[HttpDelete("/./v2/v2cat/{namespace}/{name}")]
 		public void Delete(string @namespace, string name, V2Cat v2cat)
 		{
 			Console.WriteLine($"\n{Request.Method} {Request.Path}\nnamespace : {@namespace}\nname : {name}\nrequest : {JsonConvert.SerializeObject(v2cat, Formatting.Indented)}");
