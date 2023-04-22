9697d83f447341916b929de201c5e54c1d730b4f
Fortune Ngwenya <fortune.ngwenya@platform45.com>
2023-04-22 13:43:16 +0200

    update

--- Models/V2CatSpec.cs
+++ Models/V2CatSpec.cs
@@ -5,6 +5,7 @@ namespace Example.Models
 	public class V2CatSpec
 	{
 		[Required]
+		[RegularExpression(@"^[a-zA-Z''-'\s]{1,5}$", ErrorMessage = "Only letters and digits not allowed")] // Allow up to 5 uppercase and lowercase 
 		public string Name2 { get; set; }
 
 		[Range(0, 10)]
