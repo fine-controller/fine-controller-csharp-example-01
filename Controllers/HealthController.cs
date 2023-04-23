a8c16201a7a3acbded2a945e20790a40d8b837f8
Fortune Ngwenya <fortune.ngwenya@platform45.com>
2023-04-23 07:33:24 +0200

    update

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Controllers
+{
+	[ApiController]
+	public class HealthController : ControllerBase
+	{
+		[HttpGet("/health")]
+		public IActionResult GetHealth()
+		{
+			return Ok();
+		}
+	}
+}
